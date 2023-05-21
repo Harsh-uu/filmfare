25b8da8110053d3abd101240e47e7d33cfff1cad
Harsh <harshguptaaa17@gmail.com>
2023-05-21 21:19:51 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from "./MovieCard";
 
-const API_URL = 'http://www.omdbapi.com?apikey=2e7a8bad'
+const API_URL = 'https://www.omdbapi.com?apikey=2e7a8bad'
 
 const App = () => {
     const [movies, setMovies] = useState([]);
