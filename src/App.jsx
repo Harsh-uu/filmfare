9b764215ab9b10c477c205ef821d8f28856d6b9b
Harsh <harshguptaaa17@gmail.com>
2023-05-22 22:02:16 +0530

    migrated to tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,69 @@
 import React, { useEffect, useState } from "react";
-import './App.css';
+import {IoSearchOutline} from "react-icons/io5"
+import {TbFaceIdError, TbLoader2} from "react-icons/tb"
 import SearchIcon from './search.svg';
 import MovieCard from "./MovieCard";
 
 const API_URL = 'https://www.omdbapi.com?apikey=2e7a8bad'
 
 const App = () => {
+
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [isLoading,setIsLoading] = useState(false);
     const searchMovies = async (title) => {
         setIsLoading(true);
         const response = await fetch(`${API_URL}&s=${title}`)
-        setIsLoading(false);
         const data = await response.json();
-
         setMovies(data.Search);
+        setIsLoading(false);
     }
     useEffect(() => { 
         searchMovies('Dragon ball');
     }, [])
 
 return(
-    <div className="app">
-    <h1>MovieLand</h1>
-
+    <div className="bg-slate-900 min-h-screen   relative">
     
-    <form className="search" onSubmit={(e) => {
+
+    <h1 className="font-black text-6xl text-center pt-8 text-teal-600 font-bebas tracking-wide">Filmfare</h1>
+    <form  
+    className="  max-w-3xl w-[60%] mx-auto my-8 relative "
+    onSubmit={(e) => {
         e.preventDefault();
         searchMovies(searchTerm)}}>
         <input 
+        className=" w-full h-16 placeholder:text-slate-400 placeholder:text-lg px-8 rounded-full bg-slate-800 text-white focus:outline-none focus:ring-2 focus:ring-white  focus:ring-opacity-20"
         placeholder="Search for movies" 
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button>
-        <img
-        src={SearchIcon}
-        alt="Search"
-        />
+        <button className=" absolute right-5 top-1/2 -translate-y-1/2">
+        <IoSearchOutline className="text-3xl text-white"/>
         </button>
     </form>
     
-        
+    
+
     {
-        isLoading ? <p className="loading">Loading...</p> : movies?.length > 0
+        isLoading ? <div className="w-full h-full absolute top-0 left-0 pointer-events-none  grid place-items-center">
+        <TbLoader2 className="text-6xl text-white  mx-auto mt-16 animate-spin" />
+        </div>   : movies?.length > 0
         ? (
-            <div className="container">
+            <div className="flex flex-wrap justify-center mx-20 pb-28 md:mx-32 lg:mx-44 gap-16 mt-28">
                 {movies.map((movie,index) => (
                     <MovieCard key={index} movie={movie}/>
                 ))}
             </div>
         ) :
         (
-            <div className="empty">
-            <h2>No movies found</h2>
-            </div>
+            <div className="w-full h-full  text-white absolute top-0 left-0 gap-y-4 pointer-events-none   flex flex-col justify-center items-center">
+        <TbFaceIdError className="text-7xl  mx-auto mt-16  " />
+        <h2 className="font-mono  text-2xl">Not Found </h2>
+        </div> 
         )
     }
 
-    <div className="container">
-    </div>
     </div>
 )
 }
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -2,18 +2,14 @@ import React from "react";
 
 const MovieCard = ({movie}) => {
     return(
-        <div className="movie">
-            <div>
-                <p>{movie.Year}</p>
+        <div className="relative bg-red-500 group hover:scale-105 transition-transform rounded-2xl overflow-clip">
+            <div className="absolute  w-full h-full bg-black/60 opacity-0 group-hover:opacity-100 transition-opacity duration-200 ease-in-out pointer-events-none ">
+                <p className="ml-4 mt-4 text-white">{movie.Year}</p>
             </div>
-
-            <div>
-                <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title}/>
-            </div>
-
-            <div>
-                <span>{movie.Type}</span>
-                <h3>{movie.Title}</h3>
+                <img  className="w-full h-full object-cover" src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title}/>
+            <div className="absolute bottom-0 left-0  bg-[#343739] group-hover:bg-transparent transition-colors duration-200 text-white w-full  py-4 pl-5">
+                <span className="uppercase tracking-widest text-sm">{movie.Type}</span>
+                <h3 className="text-lg font-semibold">{movie.Title}</h3>
             </div>
         </div>
     )
